ed7a5334e67a9b7f19ee8b7f17c58d3786fc61c9
deeets <59213042+deeets@users.noreply.github.com>
2019-12-24 17:40:08 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () { // Makes it possible to start to manipulate the document
+  $('#sizePicker').submit(function makeGrid(grid) {  // Creates grid upon clicking the button 'Submit'
+    $('table tr').remove(); // Lets grid be cleared when hitting the 'Submit' button again
+    var row_input = $('#input_height').val(); // Allows user to add a chosen value inside the input box to add rows 
+    var col_input = $('#input_width').val(); // Allows user to add a chosen value inside the 2nd input box to add columns
+    for (var i = 1; i <= row_input; i++) { 
+      $('table').append("<tr></tr>"); // This loop creates a row of cells
+      for (var j = 1; j <= col_input; j++) {
+        $('tr:last').append("<td></td>"); // This loop adds a cell after every row
+        $('td').attr("class", 'cells') // For every 'td', a class of 'cells' is created
+      }
+    }
+    grid.preventDefault(); // Prevents grid to be deleted after it is created
+
+    $('.cells').click(function (event) { // The function allows user to color a cell on click
+      var paint = $('#colorPicker').val();
+      $(event.target).css('background-color', paint); // Lets the chosen color on a click event to be added to grid
+    });
+  }); 
+});
